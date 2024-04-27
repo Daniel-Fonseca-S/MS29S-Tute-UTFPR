185159463c67f4e8a8d9e5480a6e63f6a67c2f2a
davi.takayama <davi.takayama@bubo.tech>
2024-04-27 19:40:52 -0300

    remoção de códigos duplicados nos arquivos de BasicTuteAi e SmartTuteAI

--- src/iams/cardgame/tute/ai/BasicTuteAI.java
+++ src/iams/cardgame/tute/ai/BasicTuteAI.java
@@ -1,164 +1,112 @@
 package iams.cardgame.tute.ai;
 
+import iams.cardgame.tute.Card;
+import iams.cardgame.tute.CardModel.Rank;
+import iams.cardgame.tute.TuteGamePlayerContext;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.ThreadLocalRandom;
 
-import iams.cardgame.tute.Card;
-import iams.cardgame.tute.TuteGamePlayerContext;
-import iams.cardgame.tute.CardModel.Rank;
+import static iams.cardgame.tute.ai.SmartTuteAI.addHighRankSameSuitCardsIfNotPinta;
 
-public class BasicTuteAI implements TuteAI
-{
-    final private TuteGamePlayerContext context;
-    
-    public BasicTuteAI(TuteGamePlayerContext context)
-    {
+public class BasicTuteAI implements TuteAI {
+    private final TuteGamePlayerContext context;
+
+    public BasicTuteAI(TuteGamePlayerContext context) {
         this.context = context;
     }
 
-    @Override
-    public Card calculatePlayerCardBegin()
-    {
-        return this.calculatePlayerCardBegin(this.context.getMyCards());
-    }
-    
-    private Card calculatePlayerCardBegin(Collection<Card> myCards)
-    {
-        ArrayList<Card> candidateCardsToThrow = new ArrayList<>(); 
+    static void getMinimumPintValue(Collection<Card> myCards, ArrayList<Card> candidateCardsToThrow, TuteGamePlayerContext context) {
+        int minimumValue = Rank.Ace.countValue;
 
-        if (candidateCardsToThrow.isEmpty())
-        {
-            if (this.context.isDeckEmpty())
-            {
-                if (candidateCardsToThrow.isEmpty())
-                {
-                    int maximumValue = 0;
-                    
-                    for (Card c : myCards)
-                    {
-                        if (c.suit != this.context.getPintaSuit())
-                        {
-                            if (c.rank.countValue == maximumValue)
-                            {
-                                candidateCardsToThrow.add(c);
-                            }
-                            else if (c.rank.countValue > maximumValue)
-                            {
-                                maximumValue = c.rank.countValue;
-                                
-                                candidateCardsToThrow.clear();
-                                
-                                candidateCardsToThrow.add(c);
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                if (candidateCardsToThrow.isEmpty())
-                {
-                    int minimumValue = Rank.Ace.countValue;
-                    
-                    for (Card c : myCards)
-                    {
-                        if (c.suit != this.context.getPintaSuit())
-                        {
-                            if (c.rank.countValue == minimumValue)
-                            {
-                                candidateCardsToThrow.add(c);
-                            }
-                            else if (c.rank.countValue < minimumValue)
-                            {
-                                minimumValue = c.rank.countValue;
-                                
-                                candidateCardsToThrow.clear();
-                                
-                                candidateCardsToThrow.add(c);
-                            }
-                        }
-                    }
-                }
-            }
+        for (Card c : myCards) {
+            if (c.suit != context.getPintaSuit()) {
+                if (c.rank.countValue == minimumValue) {
+                    candidateCardsToThrow.add(c);
+                } else if (c.rank.countValue < minimumValue) {
+                    minimumValue = c.rank.countValue;
 
-            if (candidateCardsToThrow.isEmpty())
-                candidateCardsToThrow.add(myCards.iterator().next());
-            
-        }
+                    candidateCardsToThrow.clear();
 
-        return candidateCardsToThrow.get(ThreadLocalRandom.current().nextInt(candidateCardsToThrow.size()));
-    }
-    
-    @Override
-    public Card calculatePlayerCardResponse(Card thrownCard)
-    {
-        Collection<Card> myCards = this.context.calculateAllowedCardsToAvoidRenuncio(thrownCard);
-        
-        ArrayList<Card> candidateCardsToThrow = new ArrayList<>();
-        
-        if (thrownCard.rank.countValue == 0)
-        {
-            for (Card c : myCards)
-            {
-                if (c.suit != this.context.getPintaSuit() &&
-                    c.suit == thrownCard.suit &&
-                    (c.rank == Rank.Ace || c.rank == Rank.V3))
-                {
                     candidateCardsToThrow.add(c);
                 }
             }
+        }
+    }
 
-            if (candidateCardsToThrow.isEmpty())
-                return this.calculatePlayerCardBegin(myCards);
-        }
-        else
-        {
-            if (candidateCardsToThrow.isEmpty())
-            {
-                for (Card c : myCards)
-                {
-                    if (c.suit != this.context.getPintaSuit() &&
-                        c.suit == thrownCard.suit &&
-                        c.rank.countValue > thrownCard.rank.countValue &&
-                        (c.rank == Rank.Ace || c.rank == Rank.V3))
-                    {
-                        candidateCardsToThrow.add(c);
-                    }
-                }
-            }
-            
-            if (candidateCardsToThrow.isEmpty())
-            {
-                for (Card c : myCards)
-                {
-                    if (c.suit == thrownCard.suit &&
-                        c.rank.countValue > thrownCard.rank.countValue)
-                    {
-                        candidateCardsToThrow.add(c);
-                    }
-                }
-            }
-            
-            if (candidateCardsToThrow.isEmpty())
-            {
-                if (thrownCard.suit != this.context.getPintaSuit())
-                {
-                    for (Card c : myCards)
-                    {
-                        if (c.suit == this.context.getPintaSuit() &&
-                            c.rank.countValue == 0)
-                        {
-                            candidateCardsToThrow.add(c);
-                        }
-                    }
+    static void getMaximumPintValue(Collection<Card> myCards, ArrayList<Card> candidateCardsToThrow, TuteGamePlayerContext context) {
+        int maximumValue = 0;
+
+        for (Card c : myCards) {
+            if (c.suit != context.getPintaSuit()) {
+                if (c.rank.countValue == maximumValue) {
+                    candidateCardsToThrow.add(c);
+                } else if (c.rank.countValue > maximumValue) {
+                    maximumValue = c.rank.countValue;
+
+                    candidateCardsToThrow.clear();
+
+                    candidateCardsToThrow.add(c);
                 }
             }
         }
-        
+    }
+
+    static void addHigherValueSuitCards(Card thrownCard, Collection<Card> myCards, ArrayList<Card> candidateCardsToThrow, TuteGamePlayerContext context) {
+        for (Card c : myCards) {
+            if (c.suit != context.getPintaSuit() &&
+                    c.suit == thrownCard.suit &&
+                    c.rank.countValue > thrownCard.rank.countValue &&
+                    (c.rank == Rank.Ace || c.rank == Rank.V3)) {
+                candidateCardsToThrow.add(c);
+            }
+        }
+    }
+
+    @Override
+    public Card calculatePlayerCardBegin() {
+        return this.calculatePlayerCardBegin(this.context.getMyCards());
+    }
+
+    private Card calculatePlayerCardBegin(Collection<Card> myCards) {
+        ArrayList<Card> candidateCardsToThrow = new ArrayList<>();
+
+        if (this.context.isDeckEmpty()) {
+            getMaximumPintValue(myCards, candidateCardsToThrow, this.context);
+        } else {
+            getMinimumPintValue(myCards, candidateCardsToThrow, this.context);
+        }
+
         if (candidateCardsToThrow.isEmpty())
             candidateCardsToThrow.add(myCards.iterator().next());
-        
+
         return candidateCardsToThrow.get(ThreadLocalRandom.current().nextInt(candidateCardsToThrow.size()));
+    }
+
+    @Override
+    public Card calculatePlayerCardResponse(Card thrownCard) {
+        Collection<Card> myCards = this.context.calculateAllowedCardsToAvoidRenuncio(thrownCard);
+
+        ArrayList<Card> candidateCardsToThrow = new ArrayList<>();
+
+        if (thrownCard.rank.countValue == 0) {
+            if (addHighRankSuitCards(thrownCard, myCards, candidateCardsToThrow))
+                return this.calculatePlayerCardBegin(myCards);
+        } else {
+            addHigherValueSuitCards(thrownCard, myCards, candidateCardsToThrow, this.context);
+
+            SmartTuteAI.addSuitableCardsToCandidates(thrownCard, myCards, candidateCardsToThrow, this.context);
+
+        }
+
+        if (candidateCardsToThrow.isEmpty())
+            candidateCardsToThrow.add(myCards.iterator().next());
+
+        return candidateCardsToThrow.get(ThreadLocalRandom.current().nextInt(candidateCardsToThrow.size()));
+    }
+
+    private boolean addHighRankSuitCards(Card thrownCard, Collection<Card> myCards, ArrayList<Card> candidateCardsToThrow) {
+        return addHighRankSameSuitCardsIfNotPinta(thrownCard, myCards, candidateCardsToThrow, this.context);
     }
 }
--- src/iams/cardgame/tute/ai/SmartTuteAI.java
+++ src/iams/cardgame/tute/ai/SmartTuteAI.java
@@ -1,164 +1,95 @@
 package iams.cardgame.tute.ai;
 
+import iams.cardgame.tute.Card;
+import iams.cardgame.tute.CardModel.Rank;
+import iams.cardgame.tute.TuteGamePlayerContext;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.ThreadLocalRandom;
 
-import iams.cardgame.tute.Card;
-import iams.cardgame.tute.TuteGamePlayerContext;
-import iams.cardgame.tute.CardModel.Rank;
+public class SmartTuteAI implements TuteAI {
+    private final TuteGamePlayerContext context;
 
-public class SmartTuteAI implements TuteAI
-{
-    final private TuteGamePlayerContext context;
-    
-    public SmartTuteAI(TuteGamePlayerContext context)
-    {
+    public SmartTuteAI(TuteGamePlayerContext context) {
         this.context = context;
     }
 
-    @Override
-    public Card calculatePlayerCardBegin()
-    {
-        return this.calculatePlayerCardBegin(this.context.getMyCards());
-    }
-    
-    private Card calculatePlayerCardBegin(Collection<Card> myCards)
-    {
-        ArrayList<Card> candidateCardsToThrow = new ArrayList<>(); 
-
-        if (candidateCardsToThrow.isEmpty())
-        {
-            if (this.context.isDeckEmpty())
-            {
-                if (candidateCardsToThrow.isEmpty())
-                {
-                    int maximumValue = 0;
-                    
-                    for (Card c : myCards)
-                    {
-                        if (c.suit != this.context.getPintaSuit())
-                        {
-                            if (c.rank.countValue == maximumValue)
-                            {
-                                candidateCardsToThrow.add(c);
-                            }
-                            else if (c.rank.countValue > maximumValue)
-                            {
-                                maximumValue = c.rank.countValue;
-                                
-                                candidateCardsToThrow.clear();
-                                
-                                candidateCardsToThrow.add(c);
-                            }
-                        }
-                    }
-                }
+    static boolean addHighRankSameSuitCardsIfNotPinta(Card thrownCard, Collection<Card> myCards, ArrayList<Card> candidateCardsToThrow, TuteGamePlayerContext context) {
+        for (Card c : myCards) {
+            if (c.suit != context.getPintaSuit() &&
+                    c.suit == thrownCard.suit &&
+                    (c.rank == Rank.Ace || c.rank == Rank.V3)) {
+                candidateCardsToThrow.add(c);
             }
-            else
-            {
-                if (candidateCardsToThrow.isEmpty())
-                {
-                    int minimumValue = Rank.Ace.countValue;
-                    
-                    for (Card c : myCards)
-                    {
-                        if (c.suit != this.context.getPintaSuit())
-                        {
-                            if (c.rank.countValue == minimumValue)
-                            {
-                                candidateCardsToThrow.add(c);
-                            }
-                            else if (c.rank.countValue < minimumValue)
-                            {
-                                minimumValue = c.rank.countValue;
-                                
-                                candidateCardsToThrow.clear();
-                                
-                                candidateCardsToThrow.add(c);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (candidateCardsToThrow.isEmpty())
-                candidateCardsToThrow.add(myCards.iterator().next());
-            
         }
 
-        return candidateCardsToThrow.get(ThreadLocalRandom.current().nextInt(candidateCardsToThrow.size()));
+        return candidateCardsToThrow.isEmpty();
     }
-    
-    @Override
-    public Card calculatePlayerCardResponse(Card thrownCard)
-    {
-        Collection<Card> myCards = this.context.calculateAllowedCardsToAvoidRenuncio(thrownCard);
-        
-        ArrayList<Card> candidateCardsToThrow = new ArrayList<>();
-        
-        if (thrownCard.rank.countValue == 0)
-        {
-            for (Card c : myCards)
-            {
-                if (c.suit != this.context.getPintaSuit() &&
-                    c.suit == thrownCard.suit &&
-                    (c.rank == Rank.Ace || c.rank == Rank.V3))
-                {
+
+    static void addSuitableCardsToCandidates(Card thrownCard, Collection<Card> myCards, ArrayList<Card> candidateCardsToThrow, TuteGamePlayerContext context) {
+        if (candidateCardsToThrow.isEmpty()) {
+            for (Card c : myCards) {
+                if (c.suit == thrownCard.suit &&
+                        c.rank.countValue > thrownCard.rank.countValue) {
                     candidateCardsToThrow.add(c);
                 }
             }
+        }
 
-            if (candidateCardsToThrow.isEmpty())
-                return this.calculatePlayerCardBegin(myCards);
-        }
-        else
-        {
-            if (candidateCardsToThrow.isEmpty())
-            {
-                for (Card c : myCards)
-                {
-                    if (c.suit != this.context.getPintaSuit() &&
-                        c.suit == thrownCard.suit &&
-                        c.rank.countValue > thrownCard.rank.countValue &&
-                        (c.rank == Rank.Ace || c.rank == Rank.V3))
-                    {
-                        candidateCardsToThrow.add(c);
-                    }
-                }
-            }
-            
-            if (candidateCardsToThrow.isEmpty())
-            {
-                for (Card c : myCards)
-                {
-                    if (c.suit == thrownCard.suit &&
-                        c.rank.countValue > thrownCard.rank.countValue)
-                    {
-                        candidateCardsToThrow.add(c);
-                    }
-                }
-            }
-            
-            if (candidateCardsToThrow.isEmpty())
-            {
-                if (thrownCard.suit != this.context.getPintaSuit())
-                {
-                    for (Card c : myCards)
-                    {
-                        if (c.suit == this.context.getPintaSuit() &&
-                            c.rank.countValue == 0)
-                        {
-                            candidateCardsToThrow.add(c);
-                        }
-                    }
+        if (candidateCardsToThrow.isEmpty() && thrownCard.suit != context.getPintaSuit()) {
+            for (Card c : myCards) {
+                if (c.suit == context.getPintaSuit() &&
+                        c.rank.countValue == 0) {
+                    candidateCardsToThrow.add(c);
                 }
             }
         }
-        
+    }
+
+    @Override
+    public Card calculatePlayerCardBegin() {
+        return this.calculatePlayerCardBegin(this.context.getMyCards());
+    }
+
+    private Card calculatePlayerCardBegin(Collection<Card> myCards) {
+        ArrayList<Card> candidateCardsToThrow = new ArrayList<>();
+
+        if (this.context.isDeckEmpty()) {
+            BasicTuteAI.getMaximumPintValue(myCards, candidateCardsToThrow, this.context);
+        } else {
+            BasicTuteAI.getMinimumPintValue(myCards, candidateCardsToThrow, this.context);
+        }
+
         if (candidateCardsToThrow.isEmpty())
             candidateCardsToThrow.add(myCards.iterator().next());
-        
+
         return candidateCardsToThrow.get(ThreadLocalRandom.current().nextInt(candidateCardsToThrow.size()));
+    }
+
+    @Override
+    public Card calculatePlayerCardResponse(Card thrownCard) {
+        Collection<Card> myCards = this.context.calculateAllowedCardsToAvoidRenuncio(thrownCard);
+
+        ArrayList<Card> candidateCardsToThrow = new ArrayList<>();
+
+        if (thrownCard.rank.countValue == 0) {
+            if (addHighRankSuitCards(thrownCard, myCards, candidateCardsToThrow))
+                return this.calculatePlayerCardBegin(myCards);
+        } else {
+            BasicTuteAI.addHigherValueSuitCards(thrownCard, myCards, candidateCardsToThrow, this.context);
+
+            addSuitableCardsToCandidates(thrownCard, myCards, candidateCardsToThrow, this.context);
+
+        }
+
+        if (candidateCardsToThrow.isEmpty())
+            candidateCardsToThrow.add(myCards.iterator().next());
+
+        return candidateCardsToThrow.get(ThreadLocalRandom.current().nextInt(candidateCardsToThrow.size()));
+    }
+
+    private boolean addHighRankSuitCards(Card thrownCard, Collection<Card> myCards, ArrayList<Card> candidateCardsToThrow) {
+        return addHighRankSameSuitCardsIfNotPinta(thrownCard, myCards, candidateCardsToThrow, this.context);
     }
 }
